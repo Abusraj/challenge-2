bc67209a8f7eaccb6488c5b4e69022e6360199dd
ABDULLAHABDULLAH-crypto <74300140+ABDULLAHABDULLAH-crypto@users.noreply.github.com>
2023-11-29 20:55:14 +0300

    Adding some features

--- challenge2.py
+++ challenge2.py
@@ -1,10 +1,17 @@
+
+import random;
 def main ( ) :
     number = RandomNumber ()
     guess =input ("Enter a number")
-    CheckUserChoice (number ,guess)
+    result=CheckUserChoice(number ,guess)
+    if(result):
+        print("Correct")
+    else:
+        print("Wrong")
+        
     return None
 def RandomNumber():
-    return None
+    return random.randrange(1,10)
 def CheckUserChoice (number,guess):
-    return None
+    return (number==guess)
 main()
