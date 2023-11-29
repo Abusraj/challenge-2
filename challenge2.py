6a68f64f45229accb4e13de49ecd33662ddf8486
Abusraj <118844139+Abusraj@users.noreply.github.com>
2023-11-29 20:43:34 +0300

    challange 2

---
+++ challenge2.py
@@ -0,0 +1,10 @@
+def main ( ) :
+    number = RandomNumber ()
+    guess =input ("Enter a number")
+    CheckUserChoice (number ,guess)
+    return None
+def RandomNumber():
+    return None
+def CheckUserChoice (number,guess):
+    return None
+main()
